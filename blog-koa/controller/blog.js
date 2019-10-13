f05df5258fd8404e26126767f0d2a9f60e012fa7
chenjiezi <892774940@qq.com>
2019-10-13 17:35:30 +0800

    blog CURD

--- blog-koa/controller/blog.js
+++ blog-koa/controller/blog.js
@@ -1,10 +1,70 @@
-const { exec, escape } = require('../db/mysql')
+const xss = require('xss')
+const { exec } = require('../db/mysql')
 
-const getList = async () => {
-  const sql = `select * from blogs`
+const getList = async (keyword) => {
+  let sql = `select * from blogs where 1=1` // 拼接 sql 小技巧
+  if (keyword) {
+    sql += `and title like '%${keyword}%'`
+  }
+  sql += `order by createtime desc;` // desc 降序 / asc 升序
   return await exec(sql)
 }
 
+const getDetail = async (id) => {
+  let sql = `select * from blogs where id=${id}`
+  const rows = await exec(sql) // sql 查询的结果为数组
+  return rows[0]
+}
+
+const newBlog = async (blogData = {}) => {
+  const title = xss(blogData.title)
+  const content = xss(blogData.content)
+  const createTime = Date.now()
+  const type = blogData.type
+  const label = blogData.label
+
+  const sql = `
+    insert into blogs (title, content, createtime, type, label)
+    values ('${title}', '${content}', ${createTime}, '${type}', '${label}');
+  `
+
+  const insertData = await exec(sql)
+  return {
+    id: insertData.insertId
+  }
+}
+
+const updateBlog = async (id, blogData = {}) => {
+  const title = xss(blogData.title)
+  const content = xss(blogData.content)
+  const type = blogData.type
+  const label = blogData.label
+
+  const sql = `
+    update blogs set title='${title}', content='${content}',
+    type='${type}', label='${label}' where id=${id};
+  `
+
+  const updateData = await exec(sql)
+  if (updateData.affectedRows > 0) {
+    return true
+  }
+  return false
+}
+
+const delBlog = async (id) => {
+  const sql = `delete from blogs where id='${id}' and author='${author}';`
+  const delData = await exec(sql)
+  if (delData.affectedRows > 0) {
+    return true
+  }
+  return false
+}
+
 module.exports = {
-  getList
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
 }
--- blog-koa/routes/blog.js
+++ blog-koa/routes/blog.js
@@ -1,12 +1,48 @@
 const router = require('koa-router')()
 const { SuccessModel, ErrorModel } = require('../model/resModel')
-const { getList } = require('../controller/blog')
+const { getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog } = require('../controller/blog')
 
 router.prefix('/api/blog')
 
 router.get('/list', async (ctx, next) => {
-  const dataList = await getList()
+  const keyword = ctx.query.keyword || '' // 标题关键词
+  const dataList = await getList(keyword)
   ctx.body = new SuccessModel(dataList)
+})
+
+router.get('/detail', async (ctx, next) => {
+  const detailData = await getDetail(ctx.query.id)
+  ctx.body = new SuccessModel(detailData)
+})
+
+router.post('/new', async (ctx, next) => {
+  const body = ctx.request.body
+  const data = await newBlog(body)
+  ctx.body = new SuccessModel(data) // 返回新博客的 id
+})
+
+router.post('/update', async (ctx, next) => {
+  const id = ctx.query.id
+  const body = ctx.request.body
+  const result = await updateBlog(id, body)
+  if (result) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('编辑博客失败~~')
+  }
+})
+
+router.get('/del', async (ctx, next) => {
+  const result = await delBlog(ctx.query.id)
+  if (result) {
+    ctx.body = new SuccessModel(result)
+  } else {
+    ctx.body = new ErrorModel('删除博客失败~~')
+  }
 })
 
 module.exports = router
