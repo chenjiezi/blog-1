64390a8a3e570f9417db478e729312fa6181f4a9
chenjiezi <892774940@qq.com>
2019-11-01 14:22:31 +0800

    页面整体布局

---
+++ blog-admin/src/layout/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  height: calc(100vh - 50px);
+  overflow-y: auto;
+  overflow-x: hidden;
+  background: rgba(0, 0, 0, 0.3);
+}
+</style>
--- blog-admin/src/store/index.js
+++ blog-admin/src/store/index.js
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isStretch: true // 侧栏显示/隐藏
   },
   mutations: {
+    isStretchCharge (state) {
+      state.isStretch = !state.isStretch
+    }
   },
   actions: {
   },
